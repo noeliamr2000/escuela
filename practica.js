139b28cb1c088f01c8fa217756cc9f02feca7bf6
noeliamr2000 <noeliamedinaramos@gmail.com>
2024-10-10 12:03:17 +0200

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,3 @@
+/ JavaScript para cambiar entre tema claro y oscuro
+document.getElementById('toggleThemeBtn').addEventListener('click', function() {
+document.body.classList.toggle('dark-theme');})
